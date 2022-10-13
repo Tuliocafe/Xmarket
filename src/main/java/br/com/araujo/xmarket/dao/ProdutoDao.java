96153ca0b16ef251780f1c362858b59bba3f34a9
Tulio Cafe <tuliocafe@yahoo.com.br>
2022-10-13 16:40:28 -0300

    Feature rota de Produto Historico Feature putMapping de produto para atualizar produto

--- src/main/java/br/com/araujo/xmarket/dao/ProdutoDao.java
+++ src/main/java/br/com/araujo/xmarket/dao/ProdutoDao.java
@@ -1,7 +1,10 @@
 package br.com.araujo.xmarket.dao;
 
+import br.com.araujo.xmarket.dto.IHistoricoPrecoProdutoDTO;
 import br.com.araujo.xmarket.model.Produto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.ArrayList;
 
@@ -9,4 +12,10 @@ public interface ProdutoDao extends CrudRepository<Produto, Integer> {
 
     public ArrayList<Produto> findByNomeContaining(String palavraPesquisada);
 
+    @Query(value = """
+            select id_historico_preco, preco_antigo, preco_novo, data_alteracao, Produto_id_produto, produto.nome_produto as produto
+            from historico_preco
+            inner join produto on (historico_preco.Produto_id_produto = produto.id_produto)
+            where Produto_id_produto = :id""", nativeQuery = true)
+    ArrayList<IHistoricoPrecoProdutoDTO> buscaHistoricoPorPreco(@Param("id") Integer id);
 }

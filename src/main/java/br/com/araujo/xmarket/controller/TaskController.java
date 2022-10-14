40d057ff0896a8df967625abf110b45c9960ae1a
Tulio Cafe <tuliocafe@yahoo.com.br>
2022-10-14 17:28:21 -0300

    Adicionando produto ao index

--- src/main/java/br/com/araujo/xmarket/controller/TaskController.java
+++ src/main/java/br/com/araujo/xmarket/controller/TaskController.java
@@ -1,18 +1,29 @@
 package br.com.araujo.xmarket.controller;
 
+import br.com.araujo.xmarket.model.Produto;
+import br.com.araujo.xmarket.service.IProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
 
 @Controller
 public class TaskController {
 
-
+    @Autowired
+    private IProdutoService service;
     @GetMapping("/index")
-    public String home() {
-        return "index";
+    public ModelAndView home() {
+        Iterable<Produto> produtos = service.recuperarTodos();
+        ModelAndView mv = new ModelAndView("index");
+        mv.addObject("produtos",produtos);
+        return mv;
     }
+
 
     @GetMapping("/login")
     public String login(){

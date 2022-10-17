b259eb7b34701ab2efb9de3b80ce2a895305e13a
Tulio Cafe <tuliocafe@yahoo.com.br>
2022-10-17 15:20:43 -0300

    Ajustado get de marca marca no index

--- src/main/java/br/com/araujo/xmarket/controller/TaskController.java
+++ src/main/java/br/com/araujo/xmarket/controller/TaskController.java
@@ -1,26 +1,31 @@
 package br.com.araujo.xmarket.controller;
 
+import br.com.araujo.xmarket.model.Marca;
 import br.com.araujo.xmarket.model.Produto;
+import br.com.araujo.xmarket.service.IMarcaService;
 import br.com.araujo.xmarket.service.IProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
 
 @Controller
 public class TaskController {
 
     @Autowired
     private IProdutoService service;
+
+    @Autowired
+    private IMarcaService serviceMarcas;
+
     @GetMapping("/index")
     public ModelAndView home() {
         Iterable<Produto> produtos = service.recuperarTodos();
         ModelAndView mv = new ModelAndView("index");
+        Iterable<Marca> marcas = serviceMarcas.buscarTodos();
         mv.addObject("produtos",produtos);
+        mv.addObject("marcas", marcas);
         return mv;
     }
 
@@ -34,5 +39,8 @@ public class TaskController {
     public String cadastro(){
        return "paginas/marca";
     }
+
+    @GetMapping("/admin")
+    public String admin(){return "paginas/administrativa";}
 
 }

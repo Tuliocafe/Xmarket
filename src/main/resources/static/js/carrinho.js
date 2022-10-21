981d2d0fc85f315b2dbf1695e7b5fe89730cbaa6
Tulio Cafe <tuliocafe@yahoo.com.br>
2022-10-21 19:12:39 -0300

    Ajuste de carrinho PUT

--- src/main/resources/static/js/carrinho.js
+++ src/main/resources/static/js/carrinho.js
@@ -1,23 +1,22 @@
 
-var product_total_amt = document.getElementById('product_total_amt');
+//var product_total_amt = document.getElementById('product_total_amt');
 var frete = document.getElementById('frete');
-// var precoTotal = document.getElementById('precoTotal');
+var idVenda = document.getElementById('idVenda');
 var codigoDesconto = document.getElementById('codigoDesconto1');
+
+var fetchId = 'http://localhost:8080/carrinhos/'
+
 // var precoUnitario = document.getElementById('precoUnitario');
 // var precoTotalItem = document.getElementById('preco');
 //     precoTotalItem.innerHTML = parseInt(precoUnitario.innerHTML);
 //     product_total_amt.innerHTML = parseInt(precoTotalItem.innerHTML);
-
+var somaValores = 0
 var dados
 var valorTotal = document.getElementById("precoTotal")
 var quantidadeIten = document.getElementById("quantidadeIten")
-var idVenda = 13
-var idUsuario = 209
+var carrinho  = {listaCarrinho: []}
 var vendaAtualizada
-var carrinho
-
-
-//teste[listaItensCarrinho] = 'novovalor'
+var idVenda = 13
 
 
 getdados()
@@ -31,8 +30,7 @@ async function calcularTotal(){
 }
 
 async function getdados(){
-
-    var endPoint = "http://localhost:8080/vendas/13"
+    var endPoint = "http://localhost:8080/vendas/" +idVenda
     init = {
             method: "GET",
             mode: 'cors',
@@ -44,65 +42,33 @@ async function getdados(){
             referrerPolicy: 'no-referrer',
         }
         var response = await fetch(endPoint, init);
-        var somaValores = 0
+
 
         dados = await response.json()
 
-//    console.log(dados)
-//        console.log(dados.listaItensCarrinho[0].precoUnitario)
-
-
-//var carrinho = {carrinho: [{ id: 1,
-//                            quantidade: 5,
-//                            precoTotal: 1495 },]
-//}
-
-
-            vendaAtualizada = {           id: idVenda,
-                                precoTotal: valorTotal,
-                                statusVendas: {
-                                    id: 3
-                                },
-                                cliente: {
-                                    id: idUsuario
-                             },
-                             listaItensCarrinho: [{
-                                       id: 13
-                                       }
-                             ],
-                            }
-
-vendaAtualizada.listaItensCarrinho = dados.listaItensCarrinho
-carrinho = dados.listaItensCarrinho
-
-
-//
          for (var i = 0; i <  dados.listaItensCarrinho.length; i ++ ){
-             somaValores += dados.listaItensCarrinho[i].precoTotal
-//             console.log(somaValores)
-//             console.log("precototal")
-//             console.log(dados.listaItensCarrinho[i].precoTotal)
-
+             somaValores += dados.listaItensCarrinho[i].precoTotal;
+             carrinho.listaCarrinho.push({id:dados.listaItensCarrinho[i].id ,quantidade: dados.listaItensCarrinho[i].quantidade,
+              venda:dados.id, produto:dados.listaItensCarrinho[i].produto.id_produto})
          }
-//        console.log(vendaAtualizada)
-        console.log(carrinho)
-//        console.log(dados)
-
-
         valorTotal.innerHTML =  somaValores
-        console.log(dados.listaItensCarrinho.length)
-
-        dados.listaItensCarrinho[0].precoTotal = 1495
         quantidadeIten.innerHTML = "Carrinho " + dados.listaItensCarrinho.length +" itens"
 
-//        console.log( dados.listaItensCarrinho[0])
 
-carrinho = dados.listaItensCarrinho[0]
-carrinho ['venda'] = {id: dados.id}
 
-carrinho.precoUnitario = 1000
-console.log(carrinho)
+
+
+//carrinho ={id: dados. }
+//carrinho  = {id: dados.id}
+//carrinho  = {produto: dados.listaItensCarrinho[0].produto.id_produto}
+
+
+//vendaAtualizada.listaCarrinho.push({id: 7})
+
+
+
 }
+//calcularTotal()
 
 //        console.log(dados.precoTotal)
 //        dados.precoTotal = 320
@@ -110,41 +76,38 @@ console.log(carrinho)
 //        console.log(dados.precoTotal)
 //        console.log(dados.listaItensCarrinho[1].quantidade)
 
-
-
 async function atualizarVenda(){
 
+         for (var i = 0; i <  carrinho.listaCarrinho.length; i ++ ){
+               vendaAtualizada = carrinho.listaCarrinho[i]
+               fetchId = fetchId + carrinho.listaCarrinho[i].id
+               console.log(fetchId)
+               console.log(vendaAtualizada)
+               putDados()
+         }
+
+//         vendaAtualizada = {id: idVenda , precoTotal: parseInt(valorTotal), cliente:{id:dados.cliente.id } , statusVendas:{id:3} }
+//         console.log(vendaAtualizada)
+
+}
 
 
-    fetch('http://localhost:8080/carrinhos/1',{
+
+
+async function putDados(){
+
+    fetch(fetchId,{
     method: 'put',
       headers: {
         'Content-Type': 'application/json',
       },
-    body: JSON.stringify(carrinho),}).then((response) => response.json())
+    body: JSON.stringify(vendaAtualizada),}).then((response) => response.json())
                    .then((result) => {
                      console.log('Success:', result);
                    })
                    .catch((error) => {
                      console.error('Error:', error);
                    });
-
-//    console.log(dadosJson)
-//    var endPointPost = "http://localhost:8080/vendas/13"
-//       init = {
-//               method: "PUT",
-//               mode: 'cors',
-//               cache: 'no-cache',
-//               credentials: 'same-origin',
-//               headers: {
-//                   'Accept': 'application/json',
-//                   'Content-Type': 'application/json'
-//               },
-//               referrerPolicy: 'no-referrer',
-//               body: teste
-//           }
-//    var response = await fetch(endPointPost, init);
-//    console.log(response)
 }
 
 
@@ -161,9 +124,9 @@ const diminuir = (incdec, valorUnitario) => {
         quantidade.value = 1;
         alert('Não é possível valores negativos');
     } else {
-       for (var i = 0; i <  dados.listaItensCarrinho.length; i ++ ){
-                  if(incdec == dados.listaItensCarrinho[i].id){
-                    dados.listaItensCarrinho[i].quantidade --
+       for (var i = 0; i <  carrinho.listaCarrinho.length; i ++ ){
+                  if(incdec == carrinho.listaCarrinho[i].id){
+                    carrinho.listaCarrinho[i].quantidade --
                   }
                 }
          quantidade.value = parseInt(quantidade.value) - 1;
@@ -176,11 +139,11 @@ const diminuir = (incdec, valorUnitario) => {
 const aumentar = (incdec, valorUnitario) => {
     var quantidade = document.getElementById(incdec);
     var valorUnitario = valorUnitario;
-    for (var i = 0; i <  dados.listaItensCarrinho.length; i ++ ){
-              if(incdec == dados.listaItensCarrinho[i].id){
-                dados.listaItensCarrinho[i].quantidade ++
-              }
-             }
+       for (var i = 0; i <  carrinho.listaCarrinho.length; i ++ ){
+                  if(incdec == carrinho.listaCarrinho[i].id){
+                    carrinho.listaCarrinho[i].quantidade ++
+                  }
+                }
 
           quantidade.value = parseInt(quantidade.value) + 1;
           valorTotal.innerHTML = parseInt(valorTotal.innerHTML) +  valorUnitario

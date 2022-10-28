559cf8beb143f406921ed6d8258a7c28c0629ae2
Tales Rodrigues Jardim <talesjardim@gmail.com>
2022-10-27 21:50:39 -0300

    JS CARRINHO DE COMPRA LOGOUT

---
+++ target/classes/static/js/cadastro.js
@@ -0,0 +1,68 @@
+var logado = localStorage.getItem('logado');
+
+if (logado) {
+     var auxCliente = localStorage.getItem("cliente");
+     var cliente = JSON.parse(auxCliente);
+
+ }
+
+var nome = document.getElementById('nome');
+var sobrenome = document.getElementById('sobrenome');
+var cpf = document.getElementById('cpf');
+var rg = document.getElementById('rg');
+var dataNascimento = document.getElementById('dataNascimento');
+var telefoneUm = document.getElementById('telefoneUm');
+var telefoneDois = document.getElementById('telefoneDois');
+var email = document.getElementById('email');
+var senha = document.getElementById('senha');
+var senhaDois = document.getElementById('senhaDois');
+var btnInscrever = document.getElementById('btnInscrever');
+
+
+var resposta
+
+btnInscrever.addEventListener('click', async function(){
+    if(senha.value== ""){
+    alert ("Senha não preenchida!")
+    } else if(senha.value!=senhaDois.value){
+    alert('Senhas não conferem')
+    }
+        else {
+    var cadastro =  {
+                           "nome": nome.value,
+                           "sobrenome": sobrenome.value,
+                           "cpf": cpf.value,
+                           "dataNascimento": dataNascimento.value,
+                           "telefoneUm": telefoneUm.value,
+                           "telefoneDois": telefoneDois.value,
+                           "rg": rg.value,
+//                           "dataCriacaoUsuario": "2022-10-17 17:57:03",
+                           "senha": senha.value,
+                           "email": email.value,
+                           "tipoUsuario": "administrador",
+                           "status": null
+                       }
+                       console.log(cadastro)
+    var init ={
+        method: 'POST',
+        headers: { "Content-Type":'application/json'},
+        body: JSON.stringify(cadastro)
+    }
+
+    var endpoint = 'http://localhost:8080/clientes'
+    console.log("teste")
+    var response = await fetch(endpoint, init)
+    console.log(response)
+
+         if(response.status== 404){
+                alert("Email já existente")
+
+                }    else if(response.status ==201) {
+                    alert("Dados cadastrados com sucesso! Faça Login")
+
+                window.location.href = "/login"
+                } else {
+                alert("Código do erro:" + response.status)
+                }
+    }
+})
